d2863174e44ab07270ae144bdf186a4dd9c6f33d
xJason21 <datnetdeveloper@gmail.com>
2017-08-19 19:16:45 +0300

    Update settings page

--- src/scripts/settings.js
+++ src/scripts/settings.js
@@ -1,30 +1,58 @@
+
 $(function() {
     settings.init();
 });
 
 var settings = function(){
 
+    // console.log(localStorage);
+    // $('#units').val(localStorage._units || $('#units').val()).on('change', function() {
+    //     debugger;
+    //     localStorage._units = $(this).val();
+    // });
+
     var init = function (){
         debugger;
-        console.log(localStorage);
-        $('#units').val(localStorage._units || $('#units').val()).on('change', function() {
-            localStorage._units = $(this).val();
-            // chrome.extension.getBackgroundPage().update();
-        });
+        var settingHeader = chrome.i18n.getMessage("settingsHeader");
+        var unitLabel = chrome.i18n.getMessage("units");
+        var saveText = chrome.i18n.getMessage("save");
+        var resetText = chrome.i18n.getMessage("reset");
+
+        $('#settings-header').text(settingHeader);
+        $('#units-label').text(unitLabel);
+        $('#save').text(saveText);
+        $('#reset').text(resetText);
+        $('#units').val(localStorage._units);
     }
 
     var onSave = function(){
         debugger;
-        var unit = $("#units").val();
-        console.log($(this));
-        $(this).text('Saved!');
-        chrome.storage.sync.set({'units': unit}, function (){
-            $('#save').text('Save Location');
-        });
+        var unit = $("input[name=units]:checked").val();
+        localStorage._units = unit;
+        var status = $('#status').show();
+        status.text('Options saved.');
+        setTimeout(function (){
+            status.fadeOut();
+        }, 1000);
+        // getCurrentLocation(function(value) {
+        //     if (!value) throw err
+
+        //     if (value) {
+        //         chrome.extension.getBackgroundPage().onUpdateBadge(value);
+        //     }
+        // })
     };
+
+    var onReset = function() {
+        localStorage.removeItem('_units');
+    }
 
     return {
         init: init,
-        onSave: onSave
+        onSave: onSave,
+        onReset: onReset
     }
 }();
+
+document.getElementById("save").addEventListener("click", settings.onSave);
+document.getElementById("reset").addEventListener("click", settings.onReset);
